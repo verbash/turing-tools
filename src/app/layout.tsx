01e597f81b0d9f82f961645a36fe0c512e23acd4
David LiaBraaten <davidliabraaten@gmail.com>
2025-05-02 09:36:54 -0700

    fix og image 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     siteName: "Turing Tools",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://turingtools.com/og-image.png",
         width: 1200,
         height: 630,
         alt: "Turing Tools Logo",
